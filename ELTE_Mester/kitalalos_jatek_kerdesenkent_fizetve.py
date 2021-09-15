8368b9af9f0f41659018fbbbe97f67193aa785e9
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2021-09-15 23:00:12 +0200

    add 3rd

---
+++ ELTE_Mester/kitalalos_jatek_kerdesenkent_fizetve.py
@@ -0,0 +1,2 @@
+# https://github.com/lezsakdomi/elte-mester-data/blob/master/Dinamikus%20programoz%C3%A1s/Kital%C3%A1l%C3%B3s%20j%C3%A1t%C3%A9k%20k%C3%A9rd%C3%A9senk%C3%A9nt%20fizetve%20***/feladat.pdf
+
