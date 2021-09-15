ea801d0bb594f0bae1f3adbde85a05d866b3e27a
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2021-09-15 11:39:32 +0200

    add two pointers

---
+++ ELTE_Mester/szamok_osszege.py
@@ -0,0 +1,29 @@
+import sys
+
+n, x = map(int, input().split(" "))
+s = list(map(int, input().split(" ")))
+
+
+for i_1 in range(n):
+  x_rem = x - s[i_1]
+
+  i_2 = 0
+  i_3 = n-1
+
+  while True:
+    if i_2 == i_1: # Nem használhatjuk többször ugyanazt a számot
+      i_2 += 1     # Átlépünk rajta
+    if i_3 == i_1: # - || -
+      i_3 -= 1     # - || -
+    if i_2 >= i_3: # Ha átlépték egymást a pointerek akkor nincs találat
+      break
+    sum = s[i_2] + s[i_3]
+    if sum == x_rem: # Találat
+      print(s[i_1], s[i_2], s[i_3])
+      sys.exit() # Kiírjuk, megállunk
+    if sum < x_rem: # Ha túl kevés az összeg bal pointert növeljük
+       i_2 += 1
+    elif sum > x_rem: # Ha túl sok az összeg jobb pointert csökkentjük
+       i_3 -= 1
+
+print(-1)
