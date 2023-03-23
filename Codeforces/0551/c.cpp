1655cfb5e98b866273fbc3d99e50cfb4993b480c
Viktória Nemkin <viktoria.nemkin@gmail.com>
2023-03-24 00:57:42 +0100

    nodebug

--- Codeforces/0551/c.cpp
+++ Codeforces/0551/c.cpp
@@ -1,13 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <numeric>
-
-#ifdef LOCAL
-  #include "../../algo/debug.h"
-#else
-  #define debug(...) 42
-#endif
-
 using namespace std;
 
 typedef long long ll;
@@ -16,7 +9,6 @@ int n, m;
 vector<ll> a_in;
 
 bool possible(ll time) {
-  debug("BEGIN ----", time);
   
   vector<ll> a = a_in;
 
@@ -31,27 +23,12 @@ bool possible(ll time) {
     ll time_left = time - (current_position + 1);
 
     while (0 < time_left && 0 <= current_position) {
-      debug("STEP ------");
-      debug(current_person);
-      debug(current_position);
-      debug(time_left);
-      debug(a);
-
       ll boxes_to_take = min(a[current_position], time_left);
       time_left -= boxes_to_take;
       a[current_position] -= boxes_to_take;
-      
-      debug("AFTER");
-      debug(boxes_to_take);
-      debug(time_left);
-      debug(a);
-
       while (0 <= current_position && !a[current_position]) --current_position;
     }
   }
-
-  debug("FINAL ------");
-  debug(a);
 
   return current_position < 0 && current_person <= m;
 }
